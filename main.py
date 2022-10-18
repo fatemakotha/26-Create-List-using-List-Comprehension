f692b139ac03307d2c5bf636bfd7c382021a249c
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-18 12:56:41 +0600

    Completed all examples

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
-# This is a sample Python script.
+#Creating List using FOR LOOP:
+numbers = [1, 2, 3]
+new_list = []
+for n in numbers:
+    new_num = n + 1
+    new_list.append(new_num)
+print(new_list) #prints: [2, 3, 4]
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#..............OR.......................................
+
+#Creating List using LIST COMPREHENSION:
+numbers = [1, 2, 3]
+new_list = [n + 1 for n in numbers]
+print(new_list) #prints: [2, 3, 4]
+#........................................................
+
+#EXAMPLE 2:
+name = "Kotha"
+neww_list = [letter for letter in name]
+print(neww_list) #prints: ['K', 'o', 't', 'h', 'a']
+#.........................................................
+#EXAMPLE 3:
+new_list = [n * 2 for n in range(1, 5)]
+print(new_list) #prints: [2, 4, 6, 8]
+
+#......................................................................................................
+
+#CONDITIONAL LIST COMPREHENSION
+#EXAMPLE 4: Make a new list that adds 1 to each item if item is > 2
+numberss = [1, 2, 3, 4, 5, 6]
+newww_list = [n + 1 for n in numberss if n > 2]
+print(newww_list) #prints: [4, 5, 6, 7]
+#.....................................................
+
+#EXAMPLE 5: prints name if it has less that 5 alophabets
+names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+short_names = [name for name in names if len(name) <= 5]
+long_names = [name.upper() for name in names if len(name) >= 5]
+
+print(short_names) #prints: ['Alex', 'Beth', 'Dave']
+print(long_names) #prints: ['CAROLINE', 'ELEANOR', 'FREDDIE']
+#......................................................................................................
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
